ab66ddf239e057aec09eea42cd63b1d561d21a73
Thiago G. Traue <t.traue@gmail.com>
2025-10-23 18:23:17 -0300

    iniciação do projeto mackmusic

---
+++ 04_Spring/mackmusic/src/main/java/br/mackmusic/mackmusic/model/User.java
@@ -0,0 +1,35 @@
+package br.mackmusic.mackmusic.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    
+}
---
+++ 04_Spring/mackmusic/src/test/java/br/mackmusic/mackmusic/MackmusicApplicationTests.java
@@ -0,0 +1,10 @@
+package br.mackmusic.mackmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MackmusicApplicationTests {
+
+
+}
